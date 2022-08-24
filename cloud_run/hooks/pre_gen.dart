3b15199dafdb19f73264c113d1fbfe24541fbe44
freitzzz <1160907@isep.ipp.pt>
2022-08-24 12:52:31 +0100

    include use firebase prompt

--- cloud_run/hooks/pre_gen.dart
+++ cloud_run/hooks/pre_gen.dart
@@ -3,8 +3,18 @@ import 'package:mason/mason.dart';
 void run(HookContext context) {
   final isNodeEnvironment = context.vars['environment'] == 'node';
 
+  var useFirebase = false;
+
+  if (isNodeEnvironment) {
+    useFirebase = context.logger.confirm(
+      '${lightGreen.wrap('?')} Will you be using Firebase?',
+      defaultValue: false,
+    );
+  }
+
   context.vars = {
     ...context.vars,
     'isNodeEnvironment': isNodeEnvironment,
+    'useFirebase': useFirebase,
   };
 }
