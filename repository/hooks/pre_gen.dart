c767a85843fafd48c2a2fb70db42fa4ba85ef9e5
freitzzz <1160907@isep.ipp.pt>
2022-08-25 10:34:26 +0100

    remove not needed vars

--- repository/hooks/pre_gen.dart
+++ repository/hooks/pre_gen.dart
@@ -3,22 +3,6 @@ import 'package:mason/mason.dart';
 
 void run(HookContext context) {
   _setFileEntitiesPathBeforeGen(context);
-  _setLicenseDetails(context);
-
-  final logger = context.logger;
-
-  final isOpenSource = context.vars['openSource'] == true;
-
-  var repositoryUrl = '';
-
-  if (isOpenSource) {
-    repositoryUrl = logger.prompt('${lightGreen.wrap('?')} Repository URL?');
-  }
-
-  context.vars = {
-    ...context.vars,
-    'repositoryUrl': repositoryUrl,
-  };
 }
 
 void _setFileEntitiesPathBeforeGen(HookContext context) {
@@ -27,20 +11,5 @@ void _setFileEntitiesPathBeforeGen(HookContext context) {
   context.vars = {
     ...context.vars,
     'fileEntitiesPathBeforeGen': [...paths],
-  };
-}
-
-void _setLicenseDetails(HookContext context) {
-  final license = context.vars['license'];
-
-  final underMIT = license == 'MIT';
-  final underGNUGPLV3 = license == 'GNUGPLV3';
-  final licenseYear = DateTime.now().year;
-
-  context.vars = {
-    ...context.vars,
-    'underMIT': underMIT,
-    'underGNUGPLV3': underGNUGPLV3,
-    'licenseYear': licenseYear,
   };
 }
