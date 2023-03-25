dbfd8b8db733ae03b06216be34391d9293660dd6
freitzzz <1160907@isep.ipp.pt>
2023-03-25 13:40:09 +0000

    chore(worker): remove post gen script and set type selector on pre gen

--- worker/hooks/pre_gen.dart
+++ worker/hooks/pre_gen.dart
@@ -1,46 +1,15 @@
-import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  _setFileEntitiesPathBeforeGen(context);
-  _setLicenseDetails(context);
-
-  final logger = context.logger;
-
-  final isOpenSource = context.vars['openSource'] == true;
-
-  var repositoryUrl = '';
-
-  if (isOpenSource) {
-    repositoryUrl = logger.prompt('${lightGreen.wrap('?')} Repository URL?');
-  }
-
-  context.vars = {
-    ...context.vars,
-    'repositoryUrl': repositoryUrl,
-  };
+  _setTypeSelector(context);
 }
 
-void _setFileEntitiesPathBeforeGen(HookContext context) {
-  final paths = Directory.current.listSync(recursive: true).map((e) => e.path);
+void _setTypeSelector(HookContext context) {
+  final type = context.vars['type'];
+  final isFetchWorker = type == 'fetch';
 
   context.vars = {
     ...context.vars,
-    'fileEntitiesPathBeforeGen': [...paths],
-  };
-}
-
-void _setLicenseDetails(HookContext context) {
-  final license = context.vars['license'];
-
-  final underMIT = license == 'MIT';
-  final underGNUGPLV3 = license == 'GNUGPLV3';
-  final licenseYear = DateTime.now().year;
-
-  context.vars = {
-    ...context.vars,
-    'underMIT': underMIT,
-    'underGNUGPLV3': underGNUGPLV3,
-    'licenseYear': licenseYear,
+    'isFetchWorker': isFetchWorker,
   };
 }
