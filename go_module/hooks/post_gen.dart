7325e6f5de171eb85a196fce8b5ea57bde36982a
RuteSantos <1160663@isep.ipp.pt>
2023-05-01 21:22:53 +0100

    chore(go_module): remove not needed functions in pre_gen hook and post_gen hook

--- go_module/hooks/post_gen.dart
+++ go_module/hooks/post_gen.dart
@@ -1,59 +1,6 @@
 import 'dart:io';
 import 'package:mason/mason.dart';
 
-const _kFileNamesPresentInBlackList = ['.DS_Store'];
 
 void run(HookContext context) {
-  _formatFiles(context);
-}
-
-void _formatFiles(final HookContext context) {
-  final logger = context.logger;
-  final directory = Directory.current;
-
-  final pathsOfFileEntitiesToIgnore =
-      context.vars['fileEntitiesPathBeforeGen'] as List;
-
-  final files = directory.listSync(recursive: true);
-
-  final progress = logger.progress('Formatting files');
-
-  final formattedFiles = <File>[];
-
-  for (final file in files) {
-    if (file is File && !pathsOfFileEntitiesToIgnore.contains(file.path)) {
-      final fileName = file.path.split(Platform.pathSeparator).last;
-
-      if (_kFileNamesPresentInBlackList.contains(fileName)) {
-        file.deleteSync();
-      } else {
-        final content = file.readAsStringSync();
-
-        final formattedContent = content.replaceAll(
-          RegExp('\n{3,}', multiLine: true),
-          '\n',
-        );
-
-        if (content.length != formattedContent.length) {
-          file.writeAsStringSync(formattedContent);
-
-          formattedFiles.add(file);
-        }
-      }
-    }
-  }
-
-  if (formattedFiles.isNotEmpty) {
-    progress.complete('Formatted ${formattedFiles.length} file(s):');
-
-    formattedFiles.forEach(
-      (file) {
-        logger.info(
-          '  ${darkGray.wrap(file.absolute.path)} ${lightGreen.wrap('(formatted)')}',
-        );
-      },
-    );
-  } else {
-    progress.complete('No files needed to format.');
-  }
 }
