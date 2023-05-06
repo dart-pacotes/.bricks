72020457a9b149e8b0715783c0e44cf41966586f
RuteSantos <1160663@isep.ipp.pt>
2023-05-06 22:10:38 +0100

    chore(go_module): format and clean dependencies in post_gen

--- go_module/hooks/post_gen.dart
+++ go_module/hooks/post_gen.dart
@@ -3,4 +3,58 @@ import 'package:mason/mason.dart';
 
 
 void run(HookContext context) {
+  if (_isGoAvailable()) {
+    _formatFiles(context);
+    _cleanDependencies(context);
+  }
+}
+
+bool _isGoAvailable() {
+  final checkCommandAvailabilityStatus = Process.runSync(
+    'go',
+    ['version'],
+    runInShell: true,
+  );
+
+  return (checkCommandAvailabilityStatus.stdout?.toString() ?? '').length > 0;
+}
+
+void _formatFiles(final HookContext context) {
+  final logger = context.logger;
+  final directory = Directory.current;
+
+  final progress = logger.progress('Formatting files');
+
+  logger.info('Running gofmt');
+
+  final goRunFormat = Process.runSync(
+    'gofmt -s -w .\\',
+    [],
+    runInShell: true,
+    workingDirectory: directory.path,
+  );
+
+  logger.info(goRunFormat.stdout);
+
+  progress.complete('Finish formatting.');
+}
+
+void _cleanDependencies(final HookContext context) {
+  final logger = context.logger;
+  final directory = Directory.current;
+
+  final progress = logger.progress('Cleaning Dependencies');
+
+  logger.info('Running go mod tidy');
+
+  final goRunTidy = Process.runSync(
+    'go mod tidy',
+    [],
+    runInShell: true,
+    workingDirectory: directory.path,
+  );
+
+  logger.info(goRunTidy.stdout);
+
+  progress.complete('Finish cleaning.');
 }
