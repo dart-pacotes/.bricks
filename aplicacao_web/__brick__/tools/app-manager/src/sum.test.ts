783ce0ff55d3d979e9f3d993b0de70a1ca7ca49c
freitzzz <1160907@isep.ipp.pt>
2024-05-14 08:23:07 +0100

    chore(aplicacao_web): delete app-manager test files

--- aplicacao_web/__brick__/tools/app-manager/src/sum.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@jest/globals';
-import sum from './sum';
-
-test('adding 40 with 2 equals 42', () => {
-	expect(sum(40, 2)).toBe(42);
-});
--- aplicacao_web/__brick__/tools/app-manager/src/sum.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Adds two signed numbers and returns the result
- * @param a - Left operand number
- * @param b - Right operand number
- * @returns The result of `a` + `b`
- */
-export default function (a: number, b: number) {
-	return a + b;
-}
